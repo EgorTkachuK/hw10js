1fafa86a3eab785f865ed28ae5c98369170f0d5e
Yehor Tkachuk <tkachuk.egor2010@gmail.com>
2024-06-29 00:41:00 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+
+// let answerFunction = function(answer){
+
+//     if (answer === true) {
+//         console.log("hello world") } 
+//          else  {console.log("Bye World")
+
+//          }
+//     }
+
+// let callbackFuction = (callback) => {
+
+// let interact = confirm("hello?")
+
+// callback(interact)
+
+// }
+
+
+
+// callbackFuction(answerFunction)
+
+
+// завдання 4
+
+
+
+// const applyCallbackToEachElement = (arr , callback) => {
+
+//     let result = []
+
+// for(let elem of arr){
+// result.push(callback(elem))
+// }
+// return result
+
+// }
+
+// const arr = [1,2,3,4,5]
+// const squareCallback = applyCallbackToEachElement( arr, ( num)  => num * num);
+
+// console.log(squareCallback)
+
+
+
+//   завдання 5
+
+// const calculateDiscoutedPrice = (price,discount,callback) => {
+
+//      let calculating = price - discount
+
+//        callback(calculating)
+
+       
+       
+//     }
+//     const showDiscountedPrice = (calculatedPrise) => {
+//         console.log(calculatedPrise)
+    
+// }
+// calculateDiscoutedPrice(100 ,10 , showDiscountedPrice)
+
